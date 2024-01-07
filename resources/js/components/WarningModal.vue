c6adee64460525ef7072fd07d02eb76b8d1ccde9
Dan Groleau <ddgroleau@gmail.com>
2024-01-07 18:00:18 -0500

    Update CI script

--- resources/js/components/WarningModal.vue
+++ resources/js/components/WarningModal.vue
@@ -4,17 +4,17 @@ defineProps<{optedIn: boolean,setOptedIn: (val:boolean)=>void}>();
 
 <template>
     <div v-if="!optedIn" class="fixed top-1 lg:top-0 bottom-0 left-0 right-0 w-screen h-screen flex items-center justify-center z-[1000] backdrop-blur-sm">
-        <div class="flex flex-col p-8 bg-black text-white lg:text-2xl gap-y-4 max-w-2xl m-4">
+        <div class="flex flex-col p-8 bg-black text-white lg:text-2xl gap-y-2 max-w-2xl m-4">
             <h2 class="mb-4 text-center text-2xl lg:text-4xl font-bold">WARNING</h2>
             <p>This app is not affiliated with, or endorsed by, the city of Portland, ME or any other government, civil, or business entity.</p>
-            <strong>THIS APP IS NOT TO BE USED BY ANYONE, EVER!</strong>
             <p>
                 This data was last updated 1/3/2024, using city-provided information
                 found <a href="https://www.portlandmaine.gov/556/Trash" target="_blank" class="underline">here</a>.
             </p>
             <p>
-                If you continue to use this app, <strong>DESPITE THE OBVIOUS PERIL,</strong> please note that
-                the data in this app could be outdated or wholly inaccurate.
+                Please note that the data in this app could be outdated or wholly inaccurate,
+                and that any device data you share may be stored in a database,
+                to include IP Address, latitude and longitude.
             </p>
             <div class="flex py-4 gap-8 flex-wrap justify-center mt-4">
                 <button @click="()=>setOptedIn(true)" class="py-2 px-4 bg-green-400 rounded text-black">I understand the peril</button>
