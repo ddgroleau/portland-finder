8e50f401836d691ad98009d7373e068110a90e94
Dan Groleau <ddgroleau@gmail.com>
2024-01-07 18:38:10 -0500

    Update legal docs

--- resources/js/components/WarningModal.vue
+++ resources/js/components/WarningModal.vue
@@ -3,7 +3,7 @@ defineProps<{optedIn: boolean,setOptedIn: (val:boolean)=>void}>();
 </script>
 
 <template>
-    <div v-if="!optedIn" class="fixed top-1 lg:top-0 bottom-0 left-0 right-0 w-screen h-screen flex items-center justify-center z-[1000] backdrop-blur-sm">
+    <div v-if="!optedIn" class="fixed top-0 bottom-0 left-0 right-0 w-screen h-screen flex items-center justify-center z-[1000] backdrop-blur-sm">
         <div class="flex flex-col p-8 bg-slate-600 border border-white rounded text-white lg:text-2xl gap-y-2 max-w-2xl m-4">
             <p>This app is not affiliated with, or endorsed by, the city of Portland, ME or any other government, civil, or business entity.</p>
             <p>
